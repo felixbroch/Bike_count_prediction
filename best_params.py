7acb7aa4e01b79ef9cdcb2a844e65de786def0ab
felix.brochier <felixbrochier12@gmail.com>
2024-12-11 18:09:45 +0100

    meilleur parametre lgbm

--- best_params.py
+++ best_params.py
@@ -1,15 +1,16 @@
-parameters_LGBM = {
-    "n_estimators": 425,
-    "learning_rate": 0.026160404378007696,
-    "max_depth": 13,
-    "num_leaves": 42,
-    "min_child_samples": 33,
-    "subsample": 0.9780988187891623,
-    "colsample_bytree": 0.5674447421613202,
-    "reg_alpha": 0.017452874384819947,
-    "reg_lambda": 0.016441626967583507,
-    "random_state": 42,
-}
+parameters_LGBM = {'n_estimators': 197,
+                   'learning_rate': 0.09796036713414666, 
+                   'max_depth': 11, 
+                   'num_leaves': 144, 
+                   'min_child_samples': 26, 
+                   'subsample': 0.6974141444683806, 
+                   'colsample_bytree': 0.885047960342077, 
+                   'reg_alpha': 0.00976316036669102, 
+                   'reg_lambda': 0.2548842907160891, 
+                   'tree_method': 'hist',
+                   'random_state': 42
+                   }
+
 
 
 parameters_RandomForest = {
